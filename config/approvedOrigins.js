1c2f9184c0e5bfe2c1902e77567ace400aff2d1e
Alfred Melson <avm@hmtgp.com>
2022-04-19 14:12:39 +0300

    corrected origin

--- config/approvedOrigins.js
+++ config/approvedOrigins.js
@@ -1,8 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const approvedOrigins = [
-    'https://develop.d2nkcnbo67qw9b.amplifyapp.com',
-    'http://192.168.1.152:3000',
-    'http://localhost:3000'
-];
-exports.default = approvedOrigins;
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+const approvedOrigins = ['https://hmtgp.com']
+exports.default = approvedOrigins
